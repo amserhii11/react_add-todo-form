20791dc8a22a1ef74969a685fce75ba4eee7756f
amserhii11 <amelin.serhii@gmail.com>
2019-09-18 18:08:55 +0300

    Updates

---
+++ precache-manifest.6e87aa1ee6d221151fa4b06f2855be14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa9ee532a94c35254e74ad5bd48ebc38",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "08848c5dd2483ff7dc44",
+    "url": "/react_add-todo-form/static/css/main.60953731.chunk.css"
+  },
+  {
+    "revision": "d0b32c672f7d75e1da83",
+    "url": "/react_add-todo-form/static/js/2.4a16f6e1.chunk.js"
+  },
+  {
+    "revision": "08848c5dd2483ff7dc44",
+    "url": "/react_add-todo-form/static/js/main.3ccc7fbf.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
